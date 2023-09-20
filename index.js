b1130d56f587628cbdba3e4381afd506bb11ac71
jefaokpta <jefaokpta@hotmail.com>
2023-09-20 17:38:43 -0300

    list ok

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
  * @author Jefferson Alves Reis (jefaokpta) < jefaokpta@hotmail.com >
  * Date: 20/09/23
  */
-import {S3Client} from '@aws-sdk/client-s3'
+import {ListBucketsCommand, S3Client} from '@aws-sdk/client-s3'
 
 const s3Client = new S3Client({
     region: 'us-east-1',
 });
-const listBuckets = async (s3) => {
-    const data = await s3Client.
-    console.log(data)
+const listBuckets = () => {
+    s3Client.send(new ListBucketsCommand({}))
+        .then((data) => {
+            console.log(data)
+        })
 }
 
 listBuckets()
